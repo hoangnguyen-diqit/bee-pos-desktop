0b6da96f55da87a9804fffec2c6bb37141888cca
Hoàng Ng <hoang.nguyen@diqit.io>
2020-08-25 17:13:41 +0700

    enhance(right nav): update

--- app/modules/order/order-history/OrderHistoryPage.tsx
+++ app/modules/order/order-history/OrderHistoryPage.tsx
@@ -1,19 +1,43 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
+import { Button } from "reactstrap";
 
 import { Header } from "../../../shared/header/Header";
 import { PageInner } from "../../../shared/page-inner/PageInner";
+import { RightnavDialog } from "../../../shared/rightnav/RightnavDialog";
 
 export default function OrderHistoryPage({
 
 }) {
 
+    const [ isOpenRightnavDialog, setIsOpenRightnavDialog ] = useState(false);
+
+    const _handleClickClick = () => {
+        setIsOpenRightnavDialog(true);
+    };
+
     return (
         <Fragment>
             <Header
+                actionComponent={
+                    <Fragment>
+                        <Button
+                            onClick={() => _handleClickClick()}
+                        >
+                            Click
+                        </Button>
+                    </Fragment>
+                }
             />
             <PageInner>
-
             </PageInner>
+            <RightnavDialog
+                isOpen={isOpenRightnavDialog}
+                toggleOpen={() => setIsOpenRightnavDialog(!isOpenRightnavDialog)}
+                value={{}}
+                onChange={() => {
+
+                }}
+            />
         </Fragment>
     )
 }
