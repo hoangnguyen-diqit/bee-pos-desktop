e0d03e1fb4076bfb1ecc26837cac5baa45c216c1
Hoàng Ng <hoang.nguyen@diqit.io>
2020-09-15 17:58:24 +0700

    enable ws

--- app/thirdparty/socket.io/SocketIOClient.tsx
+++ app/thirdparty/socket.io/SocketIOClient.tsx
@@ -1,23 +1,23 @@
-import React, { useContext } from 'react';
-// import SocketIO from 'socket.io-client';
+import React, { useContext, useEffect } from 'react';
+import SocketIO from 'socket.io-client';
 
 import { SocketContext } from './SocketContext';
-// import { warning, debug } from './utils';
+import { warning, debug } from './utils';
 import { AppContext } from '../../AppContext';
 import SocketIOEvent from './SocketIOEvent';
 
-// function mergeOptions(options = {}) {
-//     const defaultOptions = {
-//         reconnection: true,
-//         reconnectionAttempts: Infinity,
-//         reconnectionDelay: 1 * 1000,
-//         reconnectionDelayMax: 10 * 1000,
-//         autoConnect: true,
-//         transports: ['polling'],
-//         rejectUnauthorized: true
-//     };
-//     return { ...defaultOptions, ...options };
-// }
+function mergeOptions(options = {}) {
+    const defaultOptions = {
+        reconnection: true,
+        reconnectionAttempts: Infinity,
+        reconnectionDelay: 1 * 1000,
+        reconnectionDelayMax: 10 * 1000,
+        autoConnect: true,
+        transports: ['polling'],
+        rejectUnauthorized: true
+    };
+    return { ...defaultOptions, ...options };
+}
 
 let socket: any = null;
 
@@ -29,50 +29,50 @@ type Props = {
 function SocketIOClient(props: Props) {
 
     const {
-        // serverAddress,
+        serverAddress,
     } = useContext(AppContext);
 
-    // useEffect(() => {
-    //     if (serverAddress) {
-    //         socket = SocketIO(serverAddress, mergeOptions(props.options));
+    useEffect(() => {
+        if (serverAddress) {
+            socket = SocketIO(serverAddress, mergeOptions(props.options));
 
-    //         socket.status = 'initialized';
+            socket.status = 'initialized';
 
-    //         socket.on('connect', () => {
-    //             socket.status = 'connected';
-    //             debug('connected');
-    //         });
+            socket.on('connect', () => {
+                socket.status = 'connected';
+                debug('connected');
+            });
 
-    //         socket.on('disconnect', () => {
-    //             socket.status = 'disconnected';
-    //             debug('disconnect');
-    //         });
+            socket.on('disconnect', () => {
+                socket.status = 'disconnected';
+                debug('disconnect');
+            });
 
-    //         socket.on('error', (err) => {
-    //             socket.status = 'failed';
-    //             warning('error', err);
-    //         });
+            socket.on('error', (err) => {
+                socket.status = 'failed';
+                warning('error', err);
+            });
 
-    //         socket.on('reconnect', (data) => {
-    //             socket.status = 'connected';
-    //             debug('reconnect', data);
-    //         });
+            socket.on('reconnect', (data) => {
+                socket.status = 'connected';
+                debug('reconnect', data);
+            });
 
-    //         socket.on('reconnect_attempt', () => {
-    //             debug('reconnect_attempt');
-    //         });
+            socket.on('reconnect_attempt', () => {
+                debug('reconnect_attempt');
+            });
 
-    //         socket.on('reconnecting', () => {
-    //             socket.status = 'reconnecting';
-    //             debug('reconnecting');
-    //         });
+            socket.on('reconnecting', () => {
+                socket.status = 'reconnecting';
+                debug('reconnecting');
+            });
 
-    //         socket.on('reconnect_failed', (error) => {
-    //             socket.status = 'failed';
-    //             warning('reconnect_failed', error);
-    //         });
-    //     }
-    // }, [ serverAddress ]);
+            socket.on('reconnect_failed', (error) => {
+                socket.status = 'failed';
+                warning('reconnect_failed', error);
+            });
+        }
+    }, [ serverAddress ]);
 
     return (
         <SocketContext.Provider value={socket}>
